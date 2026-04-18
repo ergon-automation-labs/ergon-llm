5f844b0c74e1a5e5c69598a9fc3c3e9908ba6e65
Abby Malson <august.malson@gmail.com>
2026-04-19 00:06:24 +0200

    bump llm bot to v0.6.30 - fix tests with new runtime v0.6.3

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,7 @@
 ExUnit.start()
 
+# Set alternative metrics port for tests to avoid port conflicts
+Application.put_env(:bot_army_runtime, :metrics_port, 19090, persist: false)
+
 # Start the application for tests
 {:ok, _} = Application.ensure_all_started(:bot_army_llm)

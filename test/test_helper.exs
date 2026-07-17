66e3cd320715a5958cd5ebe598790d1e743a0e4c
Abby Malson <august.malson@gmail.com>
2026-07-17 16:23:24 -0600

    fix: update to use bot_army_library_runtime instead of bot_army_runtime

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,7 +2,7 @@ ExUnit.configure(exclude: [:integration, :load, :nats_live])
 ExUnit.start()
 
 # Set alternative metrics port for tests to avoid port conflicts
-Application.put_env(:bot_army_runtime, :metrics_port, 19_090, persist: false)
+Application.put_env(:bot_army_library_runtime, :metrics_port, 19_090, persist: false)
 
 # Start the application for tests
 {:ok, _} = Application.ensure_all_started(:bot_army_llm)

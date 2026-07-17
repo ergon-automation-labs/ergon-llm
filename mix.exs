e65b3942ae740606916ca3326d77234c8a71b7e5
Abby Malson <august.malson@gmail.com>
2026-07-17 06:01:24 -0600

    test: v0.7.11 - full pipeline test on LLM

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BotArmyLlm.MixProject do
   def project do
     [
       app: :bot_army_llm,
-      version: "0.7.10",
+      version: "0.7.11",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
